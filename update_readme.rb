2f87f5fd08a3787d4f179a96b2830ccf77929659
Veeraya Lekchaoum <114289600+VaLin4637@users.noreply.github.com>
2024-12-05 21:02:31 +0700

    Create update_readme.rb

---
+++ update_readme.rb
@@ -0,0 +1,24 @@
+require 'yaml'
+
+# โหลดข้อมูลจาก _config.yml
+config = YAML.load_file('_config.yml')
+
+# สร้างเนื้อหาที่จะเพิ่มใน README.md
+readme_content = <<-MD
+# Project Title
+
+## ข้อมูลจาก `_config.yml`
+- **Contact Email**: #{config['contact_email']}
+
+
+### Social Media Links:
+- [LinkedIn](#{config['social_media']['linkedin']})
+- [GitHub](#{config['social_media']['github']})
+MD
+
+# เขียนเนื้อหาลงใน README.md
+File.write('README.md', readme_content)
+File.write('risk_transference.md', readme_content)
+File.write('projectcs.md', readme_content)
+
+puts "successfully!"
